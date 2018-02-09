38d5fc9e781cc55ddd4e5ac75e489484a0b0b054
Per Bockman <per.bockman@gmail.com>
2018-02-09 15:51:08 +0100

    Added non-synched sound playback

--- public/main.js
+++ public/main.js
@@ -1,6 +1,7 @@
 const $form = document.querySelector('form');
 const $text = document.querySelector('[type="text"]')
 const $textarea = document.querySelector('textarea');
+const $player = document.querySelector('audio');
 
 $form.addEventListener('submit', (event) => {
   event.preventDefault();
@@ -8,29 +9,26 @@ $form.addEventListener('submit', (event) => {
   const payload = {
     text: $text.value
   };
-  const data = new FormData();
-
-  data.append('json', JSON.stringify(payload));
 
   const requestOptions = {
     method: 'POST',
-    body: JSON.stringify(data),
-    headers: new Headers({
-      'Content-Type': 'application/json'
-    })
   };
 
-  fetch('/synthesize', requestOptions)
+  fetch('/synthesize/text?text=' + encodeURIComponent($text.value), requestOptions)
     .then(res => res.json())
     .then(({ text, speechMarkTypes }) => {
       $textarea.textContent = text;
       $textarea.focus();
 
+      $player.src = '/synthesize/audio?text=' + encodeURIComponent(text) 
+      $player.play()
+      console.log("player", $player)
       speechMarkTypes.forEach(({ start, end, time }) => {
         setTimeout(() => {
           $textarea.setSelectionRange(start, end)
         }, time)
       });
+      
     })
     .catch(err => console.error(err));
 });
