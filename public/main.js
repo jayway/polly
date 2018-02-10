251a5137a49d9a3d84a637705d9c0995fe5de834
Joacim Löwgren <joacim.lowgren@gmail.com>
2018-02-10 10:44:29 +0100

    Clean up

--- public/main.js
+++ public/main.js
@@ -6,29 +6,37 @@ const $player = document.querySelector('audio');
 $form.addEventListener('submit', (event) => {
   event.preventDefault();
 
-  const payload = {
-    text: $text.value
-  };
-
   const requestOptions = {
     method: 'POST',
+    body: JSON.stringify({ text: $text.value }),
+    headers: {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json',
+    }
   };
 
-  fetch('/synthesize/text?text=' + encodeURIComponent($text.value), requestOptions)
+  fetch('/synthesize', requestOptions)
     .then(res => res.json())
-    .then(({ text, speechMarkTypes }) => {
-      $textarea.textContent = text;
+    .then(({ audioStream, speechMarkTypes }) => {
+      $textarea.textContent = $text.value;
       $textarea.focus();
 
-      $player.src = '/synthesize/audio?text=' + encodeURIComponent(text) 
-      $player.play()
-      console.log("player", $player)
+      // Highlight text as it is spoken (not really)
       speechMarkTypes.forEach(({ start, end, time }) => {
         setTimeout(() => {
           $textarea.setSelectionRange(start, end)
         }, time)
       });
-      
+
+      // Play the audio stream
+      // Ref: https://stackoverflow.com/a/41173858/517528
+      const uInt8Array = new Uint8Array(audioStream.data);
+      const arrayBuffer = uInt8Array.buffer;
+      const blob = new Blob([arrayBuffer]);
+      const url = URL.createObjectURL(blob);
+
+      $player.src = url;
+      $player.play();
     })
     .catch(err => console.error(err));
 });
